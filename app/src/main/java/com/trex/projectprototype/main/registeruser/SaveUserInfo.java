5d5863836fcec76a8555ea1e3dd56f6e860243c0
T-Rex <noip9211@gmail.com>
2020-06-14 00:44:08 +0530

    first commit

---
+++ app/src/main/java/com/trex/projectprototype/main/registeruser/SaveUserInfo.java
@@ -0,0 +1,13 @@
+package com.trex.projectprototype.main.registeruser;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SaveUserInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_save_user_info);
+    }
+}
